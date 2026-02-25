7e813cb170fb4b1ff5a74459c60a69f119310463
Sandeep Kumar <84sandeepsiwani@gmail.com>
2026-02-25 13:18:32 +0530

    newpage

--- webint/src/Header.jsx
+++
@@ -1,8 +0,0 @@
-export const Header = () => {
-  return (
-    <header className="header">
-      <img src="" alt="" />
-      <div className="div">I am header for testing </div>
-    </header>
-  );
-};
